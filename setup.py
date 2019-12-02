56dfae1e81bad5260179659cc37548faf4db900f
knikita <kakurnikovnikita@gmail.com>
2019-12-03 00:36:39 +0300

    package wrapper

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='klapeyron_py_utils',
+     version='0.0.1',
+     author="Nikita Klapeyron",
+     author_email="nikitaklapeyron@gmail.com",
+     description="Just my python reusable code",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/klapeyron5/klapeyron_py_utils",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "Operating System :: OS Independent",
+     ],
+ )
