9da85e6165195c842188dfbf5e0373817cf35519
knikita <kakurnikovnikita@gmail.com>
2020-01-08 14:06:54 +0300

    v0.2.5

--- klapeyron_py_utils/tensorflow/export_tf1.py
+++ klapeyron_py_utils/tensorflow/export_tf1.py
@@ -2,24 +2,28 @@ import tensorflow as tf
 assert tf.__version__[0] == '1'
 
 
-def export_tf1(session, out_t, export_dir='./export'):
-    out_t_name = out_t.split(':')[0]
+def export_tf1(session, in_tnsr_fullname, out_tnsr_fullname, export_dir='./export'):
+    assert isinstance(str, in_tnsr_fullname)
+    assert isinstance(str, out_tnsr_fullname)
 
-    output_graph_def = tf.graph_util.convert_variables_to_constants(session, session.graph.as_graph_def(), [out_t_name])
+    in_tnsr_name = in_tnsr_fullname.split(':')[0]
+    out_tnsr_name = out_tnsr_fullname.split(':')[0]
+
+    output_graph_def = tf.graph_util.convert_variables_to_constants(session, session.graph.as_graph_def(), [out_tnsr_name])
     tf.train.write_graph(output_graph_def, './tmp_write_graph', name='tmp_write_graph', as_text=False)
 
     tf.reset_default_graph()
     graph_def = tf.GraphDef()
     with tf.gfile.GFile('./tmp_write_graph/tmp_write_graph', "rb") as f:
         graph_def.ParseFromString(f.read())
-    [out] = tf.import_graph_def(graph_def, name="", return_elements=[out_t])
+    [out] = tf.import_graph_def(graph_def, name="", return_elements=[out_tnsr_fullname])
     g = out.graph
 
     builder = tf.saved_model.builder.SavedModelBuilder(export_dir)
 
     with tf.Session(graph=g) as sess:
-        input_signatures, output_signatures = {'Placeholder': g.get_tensor_by_name('Placeholder:0')}, {
-            out_t_name: g.get_tensor_by_name(out_t)}
+        input_signatures, output_signatures = {in_tnsr_name: g.get_tensor_by_name(in_tnsr_fullname)}, {
+            out_tnsr_name: g.get_tensor_by_name(out_tnsr_fullname)}
 
         signature = tf.saved_model.signature_def_utils.predict_signature_def(input_signatures, output_signatures)
 
