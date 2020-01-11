22b48995ec33f0f132fd8b0b44ab1cf788f51972
knikita <kakurnikovnikita@gmail.com>
2020-01-11 10:49:52 +0300

    v0.3.7

--- klapeyron_py_utils/video/ffmpeg.py
+++ klapeyron_py_utils/video/ffmpeg.py
@@ -2,12 +2,13 @@ import os
 import glob
 import numpy as np
 from klapeyron_py_utils.tmp_folders.tmp_folders import new_tmp, Tmp_erase_protection
+from klapeyron_py_utils.types.common_types import is_any_int
 
 
 video_extensions = ('.mp4', '.avi', '.mov', '.MOV')
 
 
-def ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, storyboard_extension='.jpg'):
+def ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, storyboard_extension='.jpg', qscale=2):
     """
     Write storyboard (frames of video) from video by ffmpeg with request:
     ffmpeg -hide_banner -loglevel panic -i movie_path -r fps_out -qscale:v 2 frames_dir/%04dextension
@@ -15,6 +16,7 @@ def ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, sto
     :param storyboard_dir: folder to store frames of storyboard
     :param storyboard_fps: final storyboard fps
     :param storyboard_extension: extension of final frames of storyboard
+    :param qscale: quality reduce (1-best, 31-worse)
     :return:
     """
     assert os.path.isfile(video_path)
@@ -22,11 +24,13 @@ def ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, sto
     assert os.path.isdir(storyboard_dir)
     assert storyboard_fps > 0
     assert storyboard_extension in {'.png', '.jpg'}
+    assert is_any_int(qscale)
+    assert 1 <= qscale <= 31
 
     request = 'ffmpeg -hide_banner -loglevel panic' \
               ' -i ' + video_path + \
               ' -r ' + str(storyboard_fps) + \
-              ' -qscale:v 2' + \
+              ' -qscale:v ' + str(qscale) + \
               ' ' + storyboard_dir + '/%04d' + storyboard_extension
     os.system(request)
 
@@ -82,7 +86,7 @@ class Tmp_erase_protection(Tmp_erase_protection):
             os.remove(os.path.join(pardir,file))
 
 
-def get_storyboard_paths_from_video(video_path, storyboard_fps, storyboard_dir='./tmp_ffmpeg', trim=None, storyboard_extension='.jpg'):
+def get_storyboard_paths_from_video(video_path, storyboard_fps, storyboard_dir='./tmp_ffmpeg', trim=None, storyboard_extension='.jpg', qscale=2):
     """
     Returns paths of frames from storyboard from video
     :param video_path: path of video to storyboard
@@ -101,8 +105,7 @@ def get_storyboard_paths_from_video(video_path, storyboard_fps, storyboard_dir='
         ffmpeg_trim_video(video_path, trim[0], trim[1], trim_path)
         assert os.path.isfile(trim_path), 'ERROR smth went wrong with trimming'
         video_path = trim_path
-    ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, storyboard_extension)
+    ffmpeg_storyboard_from_video(video_path, storyboard_dir, storyboard_fps, storyboard_extension, qscale)
+    # TODO remove trimmed video
     storyboard_paths = glob.glob(storyboard_dir + '/*' + storyboard_extension)
     return storyboard_paths
-
-get_storyboard_paths_from_video('E:\cos\soccer/video/soc-fifa1-big1.mp4', 20)
