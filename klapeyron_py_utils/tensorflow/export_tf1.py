4c32d1e1ff3f43f547e684bdd0e1b33cf10f65e6
knikita <nikitaklapeyron@gmail.com>
2020-03-11 17:06:41 +0300

    recover export out_tensors

--- klapeyron_py_utils/tensorflow/export_tf1.py
+++ klapeyron_py_utils/tensorflow/export_tf1.py
@@ -35,7 +35,7 @@ def export_tf1(session, in_tnsr_fullname, out_tnsr_fullname, export_dir='./expor
 
 def export_tf1_exp(session, in_tnsr_fullname, out_tnsrS_fullname, export_dir='./export'):
     assert isinstance(in_tnsr_fullname, str)
-    assert isinstance(out_tnsrS_fullname, list)
+    assert all([isinstance(out_tnsr_fullname, str) for out_tnsr_fullname in out_tnsrS_fullname])
 
     in_tnsr_name = in_tnsr_fullname.split(':')[0]
     out_tnsrS_name = [out_tnsr_fullname.split(':')[0] for out_tnsr_fullname in out_tnsrS_fullname]
@@ -53,7 +53,6 @@ def export_tf1_exp(session, in_tnsr_fullname, out_tnsrS_fullname, export_dir='./
         output_signatures = {}
         for out_tnsr_name, out_tnsr_fullname in zip(out_tnsrS_name, out_tnsrS_fullname):
             output_signatures[out_tnsr_name] = g.get_tensor_by_name(out_tnsr_fullname)
-
         signature = tf.saved_model.signature_def_utils.predict_signature_def(input_signatures, output_signatures)
 
         builder.add_meta_graph_and_variables(
