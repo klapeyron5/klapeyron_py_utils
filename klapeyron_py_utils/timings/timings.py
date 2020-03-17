0d7942dfab8167f97ab47d970f7148d270b5521c
knikita <nikitaklapeyron@gmail.com>
2020-03-17 12:02:25 +0300

    SEResNet34_v2

--- klapeyron_py_utils/timings/timings.py
+++ klapeyron_py_utils/timings/timings.py
@@ -6,12 +6,13 @@ def get_time(f, f_args=[], f_kwargs={}, n_avg=100, warmup=True):
     """
     Measures working time of any function
     :param f: function to be measured
-    :param n_avg: number of times to average single function execution
-    :param warmup: blank launch of f befor time measuring
-    :param f_kwargs: measured function args to pass
+    :param f_args: the measured function *args to pass
+    :param f_kwargs: the measured function *kwargs to pass
+    :param n_avg: number of times to average the single function execution
+    :param warmup: blank launch of f before time measuring
     :return: n_avg times averaged execution time of function f
     """
-    assert hasattr(f, '__call__')
+    assert callable(f)
     common_types.is_any_int(n_avg)
     assert n_avg > 0
     if warmup: f(*f_args, **f_kwargs)
